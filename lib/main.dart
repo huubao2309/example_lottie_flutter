35fc588db7cd60ee86054a58ba174e4d55d52ab3
Huu Bao Nguyen <huubao2309@gmail.com>
2020-03-21 00:03:18 +0700

    commmit for create project

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lottie Lib'),
+      ),
+      body: Center(
+        child: Text('MyHomePage'),
+      ),
+    );
+  }
+}
